f864f9dc541f171609bf97323a8c46472ccdcdda
cmenedes <cmenedes@yahoo.com>
2020-03-24 16:05:36 -0400

    fix replaceUrl bug - accounts for no env var being present

--- replaceUrl.js
+++ replaceUrl.js
@@ -2,7 +2,10 @@ require('dotenv').config()
 
 const fs = require('fs')
 const csv = fs.readFileSync('./dist/data/content.csv', 'utf8')
-const newCsv = `${csv}`.replace(/cc_url\,/, `cc_url,"${process.env.CC_URL}"`)
-fs.writeFile('./dist/data/content.csv', newCsv, err => {
-  if(err) console.error(err)
-})
+if (!`${process.env.CC_URL}`) {
+  const newCsv = `${csv}`.replace(/cc_url\,/, `cc_url,"${process.env.CC_URL}"`)
+  fs.writeFile('./dist/data/content.csv', newCsv, err => {
+    if(err) console.error(err)
+  })
+}
+
