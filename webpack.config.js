ef4c5e5192ee8eaa75c577e14ad87ed69cb6c1bf
cmenedes <cmenedes@yahoo.com>
2020-03-31 15:25:25 -0400

    modify webpack config to copy additonal html content to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,9 @@
-module.exports = require('nyc-build-helper').config.defaultWebpackConfig(__dirname)
+module.exports = require('nyc-build-helper').config.defaultWebpackConfig(
+  __dirname,
+  {
+      copyOptions: [{
+          from: 'src',
+          test: /\.html$/
+      }]
+  }
+)
