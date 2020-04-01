8f5cd86551a56c97f98409eb9fee65c068b6f1a9
timkeane <t.d.keane@gmail.com>
2020-04-01 11:44:07 -0400

    test that the value exists rather than the string representation of the value

--- replaceUrl.js
+++ replaceUrl.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const fs = require('fs')
 const csv = fs.readFileSync('./dist/data/content.csv', 'utf8')
-if (`${process.env.CC_URL}`) {
+if (process.env.CC_URL) {
   const newCsv = `${csv}`.replace(/cc_url\,/, `cc_url,"${process.env.CC_URL}"`)
   fs.writeFile('./dist/data/content.csv', newCsv, err => {
     if(err) console.error(err)
