52b41a1bf26507b23b88c67bbe13dfba908e92da
Chen <cchen@doitt.nyc.gov>
2020-06-17 14:36:34 -0400

    hours regex for FME attribute validation

---
+++ __tests__/FMEHours.test.js
@@ -0,0 +1,28 @@
+const HOURS = /^[0-9](:[0-9][0-9])?(a|p)-[0-9](:[0-9][0-9])?(a|p)$/i
+
+test('open close hours for Cooling Centers', () => {
+    // pristine format
+    expect(HOURS.test('9a-5p')).toBe(true)
+
+    // but with an extra space 
+    expect(HOURS.test('9a- 5p')).toBe(false)
+    expect(HOURS.test('9 a-5p')).toBe(false)
+    expect(HOURS.test('9a- 5p ')).toBe(false)
+    expect(HOURS.test(' 9a- 5p')).toBe(false)
+
+    // with minutes at start hours
+    expect(HOURS.test('9:30a-5p')).toBe(true)
+    expect(HOURS.test('9:30am-5p')).toBe(false)
+
+     // with minutes at close hours
+     expect(HOURS.test('9a-5:30p')).toBe(true)
+     expect(HOURS.test('9a-5:30pm')).toBe(false)
+ 
+     // with both minutes in start and close hours
+     expect(HOURS.test('9:30a-5:00p')).toBe(true)
+     expect(HOURS.test('9:30am-5:30pm')).toBe(false)
+
+     // no any other alphabetta in the hours string
+     expect(HOURS.test('k9a-5p')).toBe(false)
+     expect(HOURS.test('9a-5py')).toBe(false)
+})
