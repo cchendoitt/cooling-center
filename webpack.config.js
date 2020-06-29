7cfd0d42f4bbbc229335b78782fea1bd0c2994bd
timkeane <t.d.keane@gmail.com>
2020-06-29 17:09:40 -0400

    fix copyOptions and replaceOptions

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const replaceOptions = [{
 if (!build.isStg && !build.isPrd && process.env.REFRESH_MINS) {
   replaceOptions.push({
     dir: 'dist/js',
-    files: ['coolingCenter.js'],
+    files: ['cooling-center.js'],
     rules: [{
       search: /REFRESH_MINS\: 5/,
       replace: 'REFRESH_MINS: ' + process.env.REFRESH_MINS
@@ -32,10 +32,7 @@ if (!build.isStg && !build.isPrd && process.env.REFRESH_MINS) {
 module.exports = require('nyc-build-helper').config.defaultWebpackConfig(
   __dirname,
   {
-      copyOptions: [{
-          from: 'src',
-          test: /\.html$/
-      }],
+      copyOptions: ['src/inactive.html', 'src/311.html'],
       replaceOptions
   }
 )
